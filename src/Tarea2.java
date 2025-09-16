85f38bea22b3f8d63ba2e4565a5bec2cbec28a10
dam <jdurancruz@danielcastelao.org>
2025-09-16 09:37:32 +0200

    Metodo para los procesadores en uso

--- src/Tarea2.java
+++ src/Tarea2.java
@@ -6,10 +6,13 @@ public class Tarea2 {
        Runtime r = Runtime.getRuntime();
         int memorialibre = Math.toIntExact(r.freeMemory()/miB); // metodo para conseguir la memoria libre en la virtual de java
         int memoriatotal = Math.toIntExact(r.totalMemory()/miB); // metodo para conseguir la memoria total en la virtual de java
-        int memoriaenuso = Math.toIntExact(r.maxMemory())/miB; // metodo para conseguir la memoria en uso
+        int memoriaenuso = Math.toIntExact(r.maxMemory()/miB); // metodo para conseguir la memoria en uso
         System.out.println("Memoria libre "+memorialibre);
         System.out.println("Memoria total "+memoriatotal);
         System.out.println("Memoria en uso "+memoriaenuso);
+
+        int procesadoresenuso = r.availableProcessors();
+        System.out.println("Procesadores en uso "+procesadoresenuso); // metodo para ver los procesadores en uso
 
     }
 }
