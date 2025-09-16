43c2aecae67da0b628d6a3f410542c34e0304dc1
dam <jdurancruz@danielcastelao.org>
2025-09-16 09:28:54 +0200

    Metodo memoria libre

--- src/Tarea2.java
+++ src/Tarea2.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+
 public class Tarea2 {
+    public static void main(String[] args) {
+
+       Runtime r = Runtime.getRuntime();
+        int memorialibre = Math.toIntExact(r.freeMemory()); // metodo para conseguir la memoria libre en la virtual de java
+
+        System.out.println("Memoria libre "+memorialibre);
+
+    }
 }
