3a24f8ad6dbb89530c0f9fcb0a4d81ef1372cef2
dam <jdurancruz@danielcastelao.org>
2025-09-16 10:00:45 +0200

    abrir el editor de texto

--- src/Tarea3.java
+++ src/Tarea3.java
@@ -1,5 +1,9 @@
-public class Tarea3 {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Tarea3 {
+    public static void main(String[] args) throws IOException {
+
+        String [] pbloc = {"gnome-text-editor"};
+        Runtime.getRuntime().exec(pbloc); // esto abre el editor de texto
     }
 }
