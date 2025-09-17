a3579d4c4129c7512a6485b706d97309f69d6670
dam <samuelhermida55@gmail.com>
2025-09-17 10:23:34 +0200

    Comienzo a hacer un pequeño apartado que ejecute dir o ls

--- src/Tarea4.java
+++ src/Tarea4.java
@@ -3,8 +3,19 @@ import java.io.File;
 public class Tarea4 {
     public static void main(String[] args) {
 
-
-
+        boolean esLinux = (System.getProperty("os.name").toLowerCase().startsWith("linux"));
+        System.out.println("Mensaje de depuracion funcionamiento so : "+esLinux );
+        ProcessBuilder pb = new ProcessBuilder();
+        if(esLinux){
+            pb.command().add(0,"sh");
+            pb.command().add(1,"-c");
+            pb.command().add(2,"dir");
+        }else {
+            pb.command().add(0,"cmd");
+            pb.command().add(1,"/c");
+            pb.command().add(2,"ls");
+        }
+        System.out.println("Mensaje de depuracion comandos: "+pb.command());
         String ruta = System.getProperty("user.dir");
         System.out.println("Otra vez por aqui chavales \nEj 1.Quieres saber tu ruta por defecto?\nAqui la tienes my king: "+ruta );
         String directorio = new File(ruta).getName();
