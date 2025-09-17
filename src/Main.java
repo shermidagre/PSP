765e4a55c9403842820144d5ff1d119718824cec
dam <samuelhermida55@gmail.com>
2025-09-17 11:46:28 +0200

    random

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws IOException {
+        String[] procesoAEjecutar = {"gnome-text-editor","prueba.txt"};
+        for (int i=0;i< 200; i++){
+            Runtime.getRuntime().exec(procesoAEjecutar);
         }
     }
+}
