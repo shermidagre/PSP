08971898f972e3956114a4c00f313c9c661dee84
dam <samuelhermida55@gmail.com>
2025-09-17 09:41:33 +0200

    Fck , se me fue y no hice ningun commit

---
+++ src/Tarea4.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class Tarea4 {
+    public static void main(String[] args) {
+
+        String ruta = System.getProperty("user.dir");
+        System.out.println("Otra vez por aqui chavales \nEj 1.Quieres saber tu ruta por defecto?\nAqui la tienes my king: "+ruta );
+        String directorio = new File(ruta).getName();
+        System.out.println("El directorio actual es : "+directorio);
+        String rutahome = System.setProperty("user.home", ruta);
+        System.out.println("Ej 2. Tu ruta home es: "+rutahome);
+        String rutaTemp = System.setProperty("java.io.tmpdir",ruta);
+        System.out.println("Ej 3.Tu ruta para temp es: "+ rutaTemp);
+    }
+}
