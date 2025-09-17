830786193f969bbfa13fab15ca2f6dbf11b476e2
dam <samuelhermida55@gmail.com>
2025-09-17 10:24:31 +0200

    Comienzo a hacer un pequeño apartado que ejecute dir o ls

--- src/Tarea4.java
+++ src/Tarea4.java
@@ -17,13 +17,13 @@ public class Tarea4 {
         }
         System.out.println("Mensaje de depuracion comandos: "+pb.command());
         String ruta = System.getProperty("user.dir");
-        System.out.println("Otra vez por aqui chavales \nEj 1.Quieres saber tu ruta por defecto?\nAqui la tienes my king: "+ruta );
+        System.out.println("Otra vez por aqui chavales \nEj 1. Quieres saber tu ruta por defecto?\nAqui la tienes my king: "+ruta );
         String directorio = new File(ruta).getName();
         System.out.println("El directorio actual es : "+directorio);
         String rutahome = System.setProperty("user.home", ruta);
         System.out.println("Ej 2. Tu ruta home es: "+rutahome);
         String rutaTemp = System.setProperty("java.io.tmpdir",ruta);
-        System.out.println("Ej 3.Tu ruta para temp es: "+ rutaTemp);
+        System.out.println("Ej 3. Tu ruta para temp es: "+ rutaTemp);
     }
 }
 
