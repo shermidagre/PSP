b1743983f105e696bf87dfd032b3bd99b0836f6d
shermidagre <samuelhermida55@gmail.com>
2025-09-23 19:32:25 +0200

    .

--- src/Tarea5.java
+++ src/Tarea5.java
@@ -1,22 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-/*
-
-
-Crea un programa en Java que esté en bucle realizando lo siguiente:
-
-1. Pide por consola al usuario un comando y sus parámetros a ejecutar
-(si fuese necesario) (Por ejemplo, ls, gnome-text-editor, open...)
-2. Lanza el proceso y obtén el código de finalización del mismo.
-3. Muestra el nombre del programa y el código de finalización del mismo.
-
-El programa finaliza cuando un usuario introduce “salir” y devolverá un código de
-salida = 0. Prueba a introducir comandos y/o parámetros inexistentes y observa el
-código de finalización.
-
- */
-
 
 public class Tarea5 {
 
@@ -34,7 +18,7 @@ public class Tarea5 {
                 break;
             }
 
-            ProcessBuilder pb = new ProcessBuilder("sh", "-c", comando);
+            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "\\c", comando);
             pb.inheritIO();
             Process p = pb.start();
             int exitCode = p.waitFor();
--- src/Tarea7.java
+++ src/Tarea7.java
@@ -6,17 +6,21 @@ import java.util.Scanner;
 public class Tarea7 {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-            ProcessBuilder pb = new ProcessBuilder("sh", "-c","cd src", "cd Tarea7.1");
+            ProcessBuilder pb = new ProcessBuilder("cmd", "\\c","dir");
             pb.inheritIO();
             Process p = pb.start();
 
             int exitCode = p.waitFor();
             System.out.println("el comando ls ha finalizado con código: " + exitCode);
+
             OutputStream os = pb.start().getOutputStream();
             OutputStreamWriter wr = new OutputStreamWriter(os);
-            wr.write("Este es el contenido del archivo src/Tarea7.1");
+            wr.write("Este es el contenido del archivo src/Tarea7.1.py");
+
             System.out.println(wr);
             wr.close();
+
+
         }
     }
 
