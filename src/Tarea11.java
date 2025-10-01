75d2dc2ec3b0397939a4a82469b0bd800a45db99
dam <samuelhermida55@gmail.com>
2025-10-01 10:03:17 +0200

    nivel2

--- src/Tarea11.java
+++ src/Tarea11.java
@@ -1,26 +1,30 @@
 import java.awt.font.TextAttribute;
-
+import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class Tarea11 extends Thread{
 
+     private int nhilos;
+     private String n;
 
-    private String n;
-
-    public Tarea11 (String nombre ,String n) {
+    public Tarea11 (String nombre ,String n,int nhilos) {
         super(nombre);
         this.n = n;
+        this.nhilos = nhilos;
     }
-    // Definimos lo que hace el hilo
+
     @Override
     public void run() {
+
         int it= 0;
         int valornumerico = Integer.parseInt(n);
-        Thread hilohijo = new Tarea11(getName(), Integer.toString(parseInt(n)+1));
+        String siguientehilo = Integer.toString(valornumerico + 1);
+        Thread hilohijo = new Tarea11(getName(), siguientehilo, nhilos);
 
-        if (valornumerico < 5) {
+        if (valornumerico < nhilos) {
             hilohijo.start();
         }
+
         while (it < 5) {
 
             long tiempoAleatorio = (long) (Math.random() * 600 + 100); // Rango de 100 a 600 ms
@@ -41,11 +45,31 @@ public class Tarea11 extends Thread{
 
 
 
-
-
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int numHilos ;
+        boolean entradaValida = false;
 
-        new Tarea11("Hilo","1").start();
+        do {
+            System.out.print("Elige número de hilos (debe ser un número entero): ");
+            if (s.hasNextInt()) {
+                numHilos = s.nextInt();
+                entradaValida = true;
+
+            } else {
+                System.out.println("⚠️");
+                System.out.println("Entrada no válida. Se asignará el valor por defecto de 5 hilos.");
+                numHilos = 5;
+                break;
+            }
+
+        } while (!entradaValida);
+
+        if (numHilos >= 1){
+            new Tarea11("Hilo", "1", numHilos).start();
+        }
+
+        s.close();
 
     }
 }
