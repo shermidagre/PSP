caa852ea43c390fd257f21403d01539c1da2c349
dam <samuelhermida55@gmail.com>
2025-10-07 11:09:57 +0200

    rematada

---
+++ src/Prueba.java
@@ -0,0 +1,17 @@
+public class Prueba extends Thread{
+
+
+    public Prueba (String nombre ) {
+        super(nombre);
+
+    }
+    public void run() {
+
+
+
+    }
+    public static void main (String[]args){
+
+
+    }
+}
